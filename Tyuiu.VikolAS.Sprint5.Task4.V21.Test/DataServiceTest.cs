47bc495dab2ee2e18a56c535ecee9295728bac9e
alex vikol <sashavikol7@gmail.com>
2025-11-10 21:47:03 +0500

    Task4

---
+++ Tyuiu.VikolAS.Sprint5.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.VikolAS.Sprint5.Task4.V21.Lib;
+using System.IO;
+
+namespace Tyuiu.VikolAS.Sprint5.Task4.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Создаем временный файл
+            string tempFile = Path.Combine(Path.GetTempPath(), "InPutDataFileTask4V0.txt");
+            File.WriteAllText(tempFile, "2.5"); // пример значения
+
+            DataService ds = new DataService();
+            double result = ds.LoadFromDataFile(tempFile);
+
+            // Вычисляем ожидаемый результат
+            double expected = Math.Round(Math.Pow(2.5, 3) * Math.Cos(2.5) + 2 * 2.5, 3);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Tyuiu.VikolAS.Sprint5.Task4.V21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Tyuiu.VikolAS.Sprint5.Task4.V21.Lib;
+
+namespace Tyuiu.VikolAS.Sprint5.Task4.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            string tempPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "InPutDataFileTask4V0.txt");
+            double result = ds.LoadFromDataFile(tempPath);
+
+            Console.WriteLine(result);
+        }
+    }
+}
